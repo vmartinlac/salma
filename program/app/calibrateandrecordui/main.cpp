1a5e0ad49f33f69035a71786c1f0c35399982a73
Victor Martin Lac <victornement@free.fr>
2018-11-01 12:32:31 +0100

    Added a few lines to test stereo stream

--- program/app/calibrateandrecordui/main.cpp
+++ program/app/calibrateandrecordui/main.cpp
@@ -3,8 +3,51 @@
 #include "VideoSystem.h"
 #include "MainWindow.h"
 
+/*
+#include <QThread>
+#include <thread>
+#include <iostream>
+
+void tmp()
+{
+    auto proc = [] ()
+    {
+        VideoSourcePtr cam = VideoSystem::instance()->createStereoAvtVideoSource(0, 1);
+
+        for(int j=0; j<1; j++)
+        {
+            if(j > 0)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+
+            cam->open();
+            for(int i=0; i<1000; i++)
+            {
+                Image im;
+                cam->trigger();
+                //std::cout << "read!" << std::endl;
+                cam->read(im);
+                if(im.isValid() == false) std::cout << "invalid" << std::endl;
+            }
+            cam->close();
+        }
+    };
+
+    VideoSystem::instance()->initialize(true);
+    proc();
+    std::cout << "switching to thread!" << std::endl;
+    std::thread t(proc);
+    t.join();
+    VideoSystem::instance()->finalize();
+    exit(0);
+}
+*/
+
 int main(int num_args, char** args)
 {
+    //tmp();
+    //
     QApplication app(num_args, args);
 
     if( VideoSystem::instance()->initialize(true) == false )
@@ -32,3 +75,4 @@ int main(int num_args, char** args)
 
     return 0;
 }
+
