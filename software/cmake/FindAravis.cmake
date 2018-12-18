7cafe886b7e5ccd5f253133529c679f28ca242f6
Victor Martin Lac <victormartinlac@gmail.com>
2018-12-18 02:26:14 +0100

    Change FindAravis.cmake so that Aravis is found correctly.

--- software/cmake/FindAravis.cmake
+++ software/cmake/FindAravis.cmake
@@ -1,8 +1,9 @@
 find_package(PkgConfig REQUIRED)
 
-pkg_search_module(ARAVIS REQUIRED aravis-0.6 glib-2.0)
+pkg_search_module(ARAVIS REQUIRED aravis-0.6)
+pkg_search_module(GLIB REQUIRED glib-2.0)
 
 add_library(Aravis INTERFACE)
-target_include_directories(Aravis INTERFACE ${ARAVIS_INCLUDE_DIRS})
-target_link_libraries(Aravis INTERFACE ${ARAVIS_LDFLAGS})
+target_include_directories(Aravis INTERFACE ${ARAVIS_INCLUDE_DIRS} ${GLIB_LIBRARIES})
+target_link_libraries(Aravis INTERFACE ${ARAVIS_LDFLAGS} ${GLIB_LDFLAGS})
 
