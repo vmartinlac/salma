7ae79b5cda2472ae50be2efa07219fb9b9c39b38
Victor Martin Lac <victornement@free.fr>
2019-03-17 20:39:49 +0100

    Nada

--- software/cmake/FindAravis.cmake
+++ software/cmake/FindAravis.cmake
@@ -1,7 +1,8 @@
 
-pkg_search_module(ARAVIS REQUIRED aravis-0.6 glib-2.0)
+pkg_search_module(LIBARAVIS REQUIRED aravis-0.6)
+pkg_search_module(LIBGLIB2 REQUIRED glib-2.0)
 
 add_library(Aravis INTERFACE)
-target_include_directories(Aravis INTERFACE ${ARAVIS_INCLUDE_DIRS})
-target_link_libraries(Aravis INTERFACE ${ARAVIS_LDFLAGS})
+target_include_directories(Aravis INTERFACE ${LIBARAVIS_INCLUDE_DIRS} ${LIBGLIB2_INCLUDE_DIRS})
+target_link_libraries(Aravis INTERFACE ${LIBARAVIS_LDFLAGS} ${LIBGLIB2_LDFLAGS})
 
